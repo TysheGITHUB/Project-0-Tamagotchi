620eee48cf05d8c5d735dcccc97ba274b4900eab
TysheGITHUB <business.tyshewils@gmail.com>
2022-03-02 11:24:38 -0500

    (added HTML )

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+// const myHeader = document.getElementById('myheader');
+// // console.log(myHeader);
+
+// const myButton = document.getElementById('my-button');
+// //console.log(myButton);
+// const addOne = () => {
+//     // litterary adding plus(+1) 
+//     //console.log(“addOne function invoke”)
+    
+//     num++
+//     myCount.innerText= num; // this invoke the method 
+//     console.log(num);
+//     };
+
+// myButton.addEventListener("click", addOne)
+
+// const myCount = document.getElementById ('my-count');
+// // console.log(myCount);
+
+// let num = 0; //<-- any number of your choice 
+
+// console.log(myCount.innerText);
+
+// // EVENT LISTENER //
+
+
+// myButton.addEventListener('click' , addOne);
