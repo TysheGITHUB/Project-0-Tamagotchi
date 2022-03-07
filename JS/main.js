51ef9e0a4757f1372b4405c2f90607202dcd27bf
TysheGITHUB <business.tyshewils@gmail.com>
2022-03-06 21:07:24 -0500

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,63 +1,85 @@
 // identify the elements in the DOM with variables in our JS
-const hungryBtn = document.querySelector("btn-Hungry");
-const soicalizeBtn = document.querySelector("btn-Soicalize");
-const sleepBtn = document.querySelector("btn-sleep");
-const bathroomBtn = document.querySelector("btn-bathroom")
-const restBtn = document.querySelector("btn-rest"); 
-const countEl = document.getElementById("count");
-
+// const hungryBtn = document.querySelector("btn-Hungry");
+// const soicalizeBtn = document.querySelector("btn-Soicalize");
+// const sleepBtn = document.querySelector("btn-sleep");
+// const bathroomBtn = document.querySelector("btn-bathroom")
+// const restBtn = document.querySelector("btn-rest"); 
+// const countEl = document.getElementById("count");
 
 class Pet {
-    constructor (age, hungry, socialzie, sleep, bathrrom ) {
-        this.age = age;
+    constructor ( hungry, socialize, sleep, bathroom ) {
         this.hungry = hungry;
-        this.socialize= socialize;
+        this.socialize = socialize;
         this.sleep= sleep;
         this.bathroom = bathroom;
     }
 };
-
 // let Hungry = 100;
 // let Socialize = 100
 // let Sleep = 100;
 // let Bathroom = 100;
-  
-function hungryClick() {
-  // console.log("hungry button clicked!");
-  myTimer = setInterval(Loop, 1000) 
-    count ++ ;
-    countEl.innerText = "Count: " + count ;
- 1000;
 
+petHungry = function () {
+    if ( startHungry % 10 === 0){
+        this.hungry++;
+    }
 
-function handleSoicalizeClick() {
-  // console.log("pause button clicked!");
-  clearInterval(myCounter);
-};
-function handleSleepClick() {
-  // console.log("stop button clicked!");
-  clearInterval(myCounter);
-  count = 0;
-  // console.log(count);
-  countEl.innerText = `Count: ${stopp}`;
-};
-function bathroomClick() {
-    // console.log("stop button clicked!");
-    clearInterval(myCounter);
-    count = 0;
-    // console.log(count);
-    countEl.innerText = `Count: " ${stopp}`;
 }
-function resetCounter(){ 
-    clearInterval(interval); 
-    i = 0; 
-    counter.innerHTML = i; 
+decreseHungry = function () {
+    if (this.hungry > 0) {
+        return this.hungry -= 1;
+    }
+}
+trackSocialize = function () {
+    if ( startSocialize % 6 === 0) {
+        this.socialize++;
+    
+    }
 }
 
-const stopTimer = () => {
-    clearInterval(setSeconds(0))
-    document.querySelector('#counter').remove()
+decreaseSoicailize = function() {
+    if (this.socialize > 0) {
+        return this.bored -= 1;
+    }
 }
+
+
+// function hungryClick() {
+//   // console.log("hungry button clicked!");
+//   myTimer = setInterval(Loop, 1000) 
+//     count ++ ;
+//     countEl.innerText = "Count: " + count ;
+//  1000;
+
+
+// function handleSoicalizeClick() {
+//   // console.log("pause button clicked!");
+//   clearInterval(myCounter);
+// };
+// function handleSleepClick() {
+//   // console.log("stop button clicked!");
+//   clearInterval(myCounter);
+//   count = 0;
+//   // console.log(count);
+//   countEl.innerText = `Count: ${stopp}`;
+// };
+// function bathroomClick() {
+//     // console.log("stop button clicked!");
+//     clearInterval(myCounter);
+//     count = 0;
+//     // console.log(count);
+//     countEl.innerText = `Count: " ${stopp}`;
+// }
+// function resetCounter(){ 
+//     clearInterval(interval); 
+//     i = 0; 
+//     counter.innerHTML = i; 
+// }
+
+// const stopTimer = () => {
+//     clearInterval(setSeconds(0))
+//     document.querySelector('#counter').remove()
+// }
 
 
 
