4dca27670f6b7608db996e275d2db18e49707f8c
TysheGITHUB <business.tyshewils@gmail.com>
2022-03-06 23:42:04 -0500

    debugging

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,7 @@
-// identify the elements in the DOM with variables in our JS
-// const hungryBtn = document.querySelector("btn-Hungry");
-// const soicalizeBtn = document.querySelector("btn-Soicalize");
-// const sleepBtn = document.querySelector("btn-sleep");
-// const bathroomBtn = document.querySelector("btn-bathroom")
-// const restBtn = document.querySelector("btn-rest"); 
-// const countEl = document.getElementById("count");
+let Hungry = 10;
+let Socialize = 6;
+let Sleep = 2;
+let Bathroom = 5;
 
 class Pet {
     constructor ( hungry, socialize, sleep, bathroom ) {
@@ -14,16 +11,12 @@ class Pet {
         this.bathroom = bathroom;
     }
 };
-let Hungry = 10;
-let Socialize = 6;
-let Sleep = 1;
-let Bathroom = 5;
 
 petHungry = function () {
     if ( startHungry % 10 === 0){
         this.hungry++;
     }
-
+    $('hungry-num').text(`Hungry: ${this.hungry}`);
 }
 decreseHungry = function () {
     if (this.hungry > 0) {
@@ -33,8 +26,8 @@ decreseHungry = function () {
 petSocialize = function () {
     if ( startSocialize % 6 === 0) {
         this.socialize++;
-    
     }
+    $('.socialize-num').text(`Socialize: ${this.socialize}`);
 }
 
 decreaseSoicailize = function() {
@@ -43,10 +36,10 @@ decreaseSoicailize = function() {
     }
 }
 petSleep = function () {
-    if(this.sleep > 1){
+    if(this.sleep > 2){
         this.sleep--;
     }
-        
+    $('.sleep-num').text(`Sleep: ${this.sleep}`);
  }
 
  decreaseSleep = function () {
@@ -67,101 +60,29 @@ bathroomCount = function () {
     if(this.bathroom > 5) {
         return this.bathroom -= 1;
     }
-
+    $('.bathroom-num').text(`Bathroom: ${this.bathroom}`);
 }
 
-// function hungryClick() {
-//   // console.log("hungry button clicked!");
-//   myTimer = setInterval(Loop, 1000) 
-//     count ++ ;
-//     countEl.innerText = "Count: " + count ;
-//  1000;
-
-
-// function handleSoicalizeClick() {
-//   // console.log("pause button clicked!");
-//   clearInterval(myCounter);
-// };
-// function handleSleepClick() {
-//   // console.log("stop button clicked!");
-//   clearInterval(myCounter);
-//   count = 0;
-//   // console.log(count);
-//   countEl.innerText = `Count: ${stopp}`;
-// };
-// function bathroomClick() {
-//     // console.log("stop button clicked!");
-//     clearInterval(myCounter);
-//     count = 0;
-//     // console.log(count);
-//     countEl.innerText = `Count: " ${stopp}`;
-// }
-// function resetCounter(){ 
-//     clearInterval(interval); 
-//     i = 0; 
-//     counter.innerHTML = i; 
-// }
-
-// const stopTimer = () => {
-//     clearInterval(setSeconds(0))
-//     document.querySelector('#counter').remove()
-// }
 
 const startEl = document.getElementById("btn-start");
-const pauseEl = document.getElementById("btn-pause");
+// const pauseEl = document.getElementById("btn-pause");
 const stopEl = document.getElementById("btn-stop-count");
 const resetBtn = document.querySelector("#resetBtn"); 
 const countEl = document.getElementById("count");
 
 let count = 0;
 let myCounter = null;
+let reset = ""
+let stop = 0;
 // console.log(typeof(myCounter));
-  
-function handleStartClick() {
-  // console.log("start button clicked!");
-  myCounter = setInterval(function(){
-    count++;
-    countEl.innerText = "Count: " + count;
-    count ++ ;
-    countEl.innerText = "Count: " + count ;
-  }, 1000);
-};
 
 
-const stopTimer = () => {
-    clearInterval(setSeconds(0))
-    document.querySelector('#counter').remove()
-}
+// const stopTimer = () => {
+//     clearInterval(setSeconds(0))
+//     document.querySelector('#counter').remove()
+// }
+// restBtn.addEventListener("click", resetCounter); 
 
-
-startEl.addEventListener('click', handleStartClick);
-pauseEl.addEventListener('click', handlePauseClick);
-stopEl.addEventListener('click', handleStopClick);
-resetBtn.addEventListener("click", resetCounter); 
-
-
-hungryBtn.addEventListener('click', () =>{
-InputEvent.vaule = paraseInt(input.vaule) > 100;
-});
-
-socializeBtn.addEventListener('click', () =>{
-InputEvent.vaule = paraseInt(input.vaule) > 100;
-});
-
-sleepBtn.addEventListener('click', () => {
-InputEvent.vaule = paraseInt(input.vaule) > 100;
-});
-
-bathroomBtn.addEventListener('click', () => {
-InputEvent.vaule = paraseInt(input.vaule) > 100;
-});
-
-
-restBtn.addEventListener("click", resetCounter); 
-
-let count = 0;
-let myCounter = null;
-// console.log(typeof(myCounter));
   
 function handleStartClick() {
   // console.log("start button clicked!");
@@ -171,10 +92,10 @@ function handleStartClick() {
   }, 1000);
 };
 
-function handleJumpClick() {
-  // console.log("pause button clicked!");
-  clearInterval(myCounter);
-};
+// function handlePauseClick() {
+//   // console.log("pause button clicked!");
+//   clearInterval(myCounter);
+// };
 
 function handleStopClick() {
   // console.log("stop button clicked!");
@@ -182,8 +103,11 @@ function handleStopClick() {
   count = 0;
   // console.log(count);
   countEl.innerText = "Count: " + count;
-};
+
+}
 
 startEl.addEventListener('click', handleStartClick);
-pauseEl.addEventListener('click', handleJumpClick);
-stopEl.addEventListener('click', handleStopClick);
+// pauseEl.addEventListener('click', handlePauseClick);
+// stopEl.addEventListener('click', stopClick);
+stopEl.addEventListener('click', handleStopClick); 
+// 
